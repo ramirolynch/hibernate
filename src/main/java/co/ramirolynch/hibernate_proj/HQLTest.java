66543f6ca079acf4f1832650276861e49de42895
ramirolynch <ramirolynch@yahoo.com>
2022-12-26 15:51:33 -0600

    hqltest added

--- src/main/java/co/ramirolynch/hibernate_proj/HQLTest.java
+++ src/main/java/co/ramirolynch/hibernate_proj/HQLTest.java
@@ -19,10 +19,28 @@ public class HQLTest {
 		SessionFactory sfactory = HibernateUtils.getSessionFactory();
 		Session session = sfactory.openSession();
 
-
-		String hql = "from Student";
-
+        /*
+		//String hql = "from Student";
+		//String hql = "from Student where studentName = 'RAMIRO'";
+		//String hql = "from Student where studentName like 'R%'";
+		//String hql = "from Student where studentId in (101,102,104)";
 		Query<Student> query = session.createQuery(hql, Student.class);
+		*/
+		
+		/*
+		String hql = "from Student where studentId between ?0 and ?1"; // positional parameters
+		Query<Student> query = session.createQuery(hql, Student.class);
+		query.setParameter(0, 100);
+		query.setParameter(1, 110);
+		*/
+		
+		/*
+		String hql = "from Student where studentId between :minsid and :maxsid"; // named parameters
+		Query<Student> query = session.createQuery(hql, Student.class);
+		query.setParameter("minsid", 50);
+		query.setParameter("maxsid", 200);
+		
+		
 		List<Student> rows = query.getResultList();
 
 		for (Student row : rows) {
@@ -34,6 +52,15 @@ public class HQLTest {
 
 			}
 		}
+		*/
+		
+		session.beginTransaction();
+		String hql = "update Student set studentName = 'Ramiro' where studentId=101";
+		Query query = session.createQuery(hql);
+		int studsUpdated = query.executeUpdate();
+		System.out.println("number of students updated: "+studsUpdated);
+		
+		session.getTransaction().commit();
 		
 		session.close();
 		sfactory.close();
